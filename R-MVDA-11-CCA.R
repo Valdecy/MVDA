4c1cf94e6799668b6f73fafba4f23dba2b07f3c0
Valdecy Pereira <valdecypereira@yahoo.com.br>
2019-01-23 10:36:10 -0200

    Update R-MVDA-11-CCA.R

--- R-MVDA-11-CCA.R
+++ R-MVDA-11-CCA.R
@@ -6,7 +6,7 @@
 # Course: Multivariate Data Analysis
 # Lesson: Canonical Correlation Analysis
 # Citation: 
-# PEREIRA, V. (2019). Project: Multivariate Data Analysis, File: R-MVDA-11-CCA.R, GitHub repository: <https://github.com/Valdecy/Multivariate_Data_Analysis>
+# PEREIRA, V. (2018). Project: Multivariate Data Analysis, File: R-MVDA-11-CCA.R, GitHub repository: <https://github.com/Valdecy/Multivariate_Data_Analysis>
 
 #############################################################################################################
 
@@ -27,21 +27,40 @@ R_xx <- cor(my_data_x)
 R_xy <- cor(my_data_x, my_data_y)
 R_yx <- cor(my_data_y, my_data_x)
 
+# Covariances
+S_yy <- cov(my_data_y)
+S_xx <- cov(my_data_x)
+S_xy <- cov(my_data_x, my_data_y)
+S_yx <- cov(my_data_y, my_data_x)
+
 R_xy %*% solve(R_yy) %*% R_yx
 R_yx %*% solve(R_xx) %*% R_xy
 
 # Lambda
 R_A <- solve(R_xx) %*% R_xy %*% solve(R_yy) %*% R_yx
-eigen(R_A)$values
-A   <- eigen(R_A)$vectors
+eigen(R_A)$values # eigenvalues of each canonical pair
+A   <- eigen(R_A)$vectors # constant ak of the canonical variables
 A_x <- eigen(R_A)$vectors[ , 1]
 A_y <- eigen(R_A)$vectors[ , 2]
 
 R_B <- solve(R_yy) %*% R_yx %*% solve(R_xx) %*% R_xy
-eigen(R_B)$values
-B   <- eigen(R_B)$vectors
+eigen(R_B)$values # eigenvalues of each canonical pair
+B   <- eigen(R_B)$vectors # constant bk of the canonical variables
 B_x <- eigen(R_B)$vectors[ , 1]
 B_y <- eigen(R_B)$vectors[ , 2]
+
+# Lambda - Covariance
+S_A <- solve(S_xx) %*% S_xy %*% solve(S_yy) %*% S_yx
+eigen(S_A)$values # eigenvalues of each canonical pair
+A_r   <- eigen(S_A)$vectors # constant ak of the canonical variables
+A_r_x <- eigen(S_A)$vectors[ , 1]
+A_r_y <- eigen(S_A)$vectors[ , 2]
+
+S_B <- solve(S_yy) %*% S_yx %*% solve(S_xx) %*% S_xy
+eigen(S_B)$values # eigenvalues of each canonical pair
+B_r   <- eigen(S_B)$vectors # constant bk of the canonical variables
+B_r_x <- eigen(S_B)$vectors[ , 1]
+B_r_y <- eigen(S_B)$vectors[ , 2]
 
 # Canonical coefficients
 CC <- diag(eigen(R_A)$values^(1/2))
@@ -50,7 +69,7 @@ CC <- diag(eigen(R_A)$values^(1/2))
 
 CC <- diag(eigen(R_B)$values^(1/2))
 
-# Variance
+# Standardized Variance
 variance_A <- (t(A) %*% R_xx %*% A)
 variance_A_x <- variance_A[1,1]
 variance_A_y <- variance_A[2,2]
@@ -59,13 +78,44 @@ variance_B <- (t(B) %*% R_yy %*% B)
 variance_B_x <- variance_B[1,1]
 variance_B_y <- variance_B[2,2]
 
+# Standardized Coeficients
 A_x_st <- A_x/(variance_A_x)^(1/2)
 A_y_st <- A_y/(variance_A_y)^(1/2)
 
 B_x_st <- B_x/(variance_B_x)^(1/2)
 B_y_st <- B_y/(variance_B_y)^(1/2)
 
-# Diagnosis - Pillai Barlett Trace
+# Original Variance
+variance_r_A <- (t(A_r) %*% S_xx %*% A_r)
+variance_r_A_x <- variance_r_A[1,1]
+variance_r_A_y <- variance_r_A[2,2]
+
+variance_r_B <- (t(B_r) %*% S_yy %*% B_r)
+variance_r_B_x <- variance_r_B[1,1]
+variance_r_B_y <- variance_r_B[2,2]
+
+# Original Coefficients
+A_r_x_st <- A_r_x/(variance_r_A_x)^(1/2)
+A_r_y_st <- A_r_y/(variance_r_A_y)^(1/2)
+
+B_r_x_st <- B_r_x/(variance_r_B_x)^(1/2)
+B_r_y_st <- B_r_y/(variance_r_B_y)^(1/2)
+
+# Diagnosis – Pillai Barlett Trace
+p <- ncol(my_data_x)
+q <- ncol(my_data_y)
+n <- nrow(my_data)
+s_PB  <- min(p, q)
+u_PB  <- (n - (q + 1) - p - 1)/2
+t_PB  <- (abs(p - (q + 1) + 1) - 1)/2
+v1_PB <- s_PB*(2*t_PB + s_PB + 1)
+v2_PB <- s_PB*(2*u_PB + s_PB + 1)
+
+G <- sum(eigen(R_A)$values)
+F_test <- (G/(s_PB - G))*(v2_PB/v1_PB)
+qf(0.975, df1 = v1_PB, df2 = v2_PB)
+
+# Diagnosis - Lawley-Hotelling Trace 
 p <- ncol(my_data_x)
 q <- ncol(my_data_y)
 n <- nrow(my_data)
@@ -73,34 +123,31 @@ s_LH  <- min(p, q)
 u_LH  <- (n - (q + 1) - p - 1)/2
 t_LH  <- (abs(p - (q + 1) + 1) - 1)/2
 v1_LH <- s_LH*(2*t_LH + s_LH + 1)
-v2_LH <- s_LH*(2*u_LH + s_LH + 1)
+v2_LH <- 2*(s_LH*u_LH + 1)
 
-G <- sum(eigen(R_A)$values)
-F_test <- (G/(s_LH - G))*(v2_LH/v1_LH)
-qf(0.95, df1 = v1_LH, df2 = v2_LH)
+LH <- sum(eigen(R_A)$values/(1-eigen(R_A)$values))
+F_test <- (LH/s_LH)*(v2_LH/v1_LH)
+qf(0.975, df1 = v1_LH, df2 = v2_LH)
 
-# Diagnosis - Lawley-Hotelling Trace 
+# Diagnosis - Wilks Lambda Test 
 p <- ncol(my_data_x)
 q <- ncol(my_data_y)
 n <- nrow(my_data)
-s_LW <- 1
+s_WL <- 1
 if ((p^2 + q^2) <= 5){
-	s_LW <- 1
+	s_WL <- 1
 	ifelse
-	s_LW  <- (((p^2)*(q^2) - 4)/((p^2) + (q^2) - 5))^(1/2)
+	s_WL  <- (((p^2)*(q^2) - 4)/((p^2) + (q^2) - 5))^(1/2)
 }
 
-u_LW  <- (p*q - 2)/2
-t_LW  <- n - (q + 1) - (p - (q + 1) + 2)/2
-v1_LW <- p*q
-v2_LW <- t_LW*s_LW - u_LW
+u_WL  <- (p*q - 2)/2
+t_WL  <- n - (q + 1) - (p - (q + 1) + 2)/2
+v1_WL <- p*q
+v2_WL <- t_WL*s_WL - u_WL
 
-# Diagnosis - Wilks Lambda Test 
 Lamb <- prod(1 - eigen(R_A)$values)
-
-Wilks <- ((1 - Lamb^(1/s_LW))/Lamb^(1/s_LW))*(v2_LW/v1_LW)
-
-qf(0.95, df1 = v1_LW, df2 = v2_LW)
+Wilks <- ((1 - Lamb^(1/s_WL))/Lamb^(1/s_WL))*(v2_WL/v1_WL)
+qf(0.975, df1 = v1_WL, df2 = v2_WL)
 
 # Diagnosis - F Rao Test 
 p <- ncol(my_data_x)
@@ -118,10 +165,8 @@ v1_RAO <- p*q
 v2_RAO <- 1 + t_RAO*s_RAO - p*q/2
 
 Lamb <- prod(1 - eigen(R_A)$values)
-
 RAO <- ((1 - Lamb^(1/s_RAO))/Lamb^(1/s_RAO))*(v2_RAO/v1_RAO)
-
-qf(0.95, df1 = v1_RAO, df2 = v2_RAO)
+qf(0.975, df1 = v1_RAO, df2 = v2_RAO)
 
 # Diagnosis - Roy's Largest Root Test
 EV <- eigen(R_A)$values/(1 - eigen(R_A)$values)
@@ -136,9 +181,6 @@ V1 <- my_data_y_st %*% B_x_st
 
 W2 <- my_data_x_st %*% A_y_st
 V2 <- my_data_y_st %*% B_y_st
-
-plot(W1,V1)
-plot(W2,V2)
 
 CE_X1_W1 <- cor(my_data_x[ , 1], W1)
 CE_X2_W1 <- cor(my_data_x[ , 2], W1)
