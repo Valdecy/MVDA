443fc86ba4f7771ea48483ace57ed8981f185063
Valdecy Pereira <valdecypereira@yahoo.com.br>
2019-08-07 22:16:46 -0300

    Update R-MVDA-10-CFA.R

--- R-MVDA-10-CFA.R
+++ R-MVDA-10-CFA.R
@@ -41,56 +41,6 @@ mvn(data = my_data, mvnTest = "mardia", univariatePlot = "qqplot")
 mvn(data = my_data, mvnTest = "mardia", univariatePlot = "histogram")
 par(mfrow = c(1,1))
 
-# Outliers
-#mvn(data = my_data, mvnTest = "mardia", multivariateOutlierMethod = "quan")
-#mvn(data = my_data, mvnTest = "mardia", multivariateOutlierMethod = "adj" ) # Adjusted Mahalanobis distance
-#outliers <- mvn(data = my_data, mvnTest = "mardia", multivariateOutlierMethod = "adj" , showNewData = TRUE)
-#my_data <- outliers$newData
-
-# Transform to Normal
-#moed_nm <- bestNormalize(my_data$Moed)
-#faed_nm <- bestNormalize(my_data$Faed)
-#famn_nm <- bestNormalize(my_data$Famin)
-#eng_nm  <- bestNormalize(my_data$Eng)
-#mat_nm  <- bestNormalize(my_data$Math)
-#soc_nm  <- bestNormalize(my_data$Soc)
-#nat_nm  <- bestNormalize(my_data$Nat)
-#voc_nm  <- bestNormalize(my_data$Vocab)
-
-# Last Resort - Binarize
-#moed_nm <- binarize(my_data$Moed) 
-#faed_nm <- binarize(my_data$Faed)
-#famn_nm <- binarize(my_data$Famin)
-#eng_nm  <- binarize(my_data$Eng)
-#mat_nm  <- binarize(my_data$Math)
-#soc_nm  <- binarize(my_data$Soc)
-#nat_nm  <- binarize(my_data$Nat)
-#voc_nm  <- binarize(my_data$Vocab)
-
-#par(mfrow = c(1, 2))
-#MASS::truehist(my_data$Vocab)
-#MASS::truehist(voc_nm$x.t)
-#par(mfrow = c(1, 1))
-
-#my_data$Moed  <- moed_nm$x.t
-#my_data$Faed  <- faed_nm$x.t
-#my_data$Famin <- famn_nm$x.t
-#my_data$Eng   <- eng_nm$x.t
-#my_data$Math  <- mat_nm$x.t
-#my_data$Soc   <- soc_nm$x.t
-#my_data$Nat   <- nat_nm$x.t
-#my_data$Vocab <- voc_nm$x.t
-
-#boxplot(log10(nat_nm$oos_preds), yaxt = 'n')
-
-# Check Mulyivariate Normality
-#mvn(data = my_data, mvnTest = "mardia")
-
-# Create Univariate Plots
-#mvn(data = my_data, mvnTest = "mardia", univariatePlot = "qqplot")
-#mvn(data = my_data, mvnTest = "mardia", univariatePlot = "histogram")
-#par(mfrow = c(1,1))
-
 # CFA - Model Specification
 cfa_model <- "
 # Measurement Model
@@ -128,7 +78,6 @@ semPaths(object = cfa_fit, what = "std", whatLabels = "std", title = FALSE, curv
 # "MLR" will compute a test statistic which is asymptotically equivalent to the Yuan-Bentler 50 lavOptions T2-star test statistic (test = "yuan.bentler.mplus"). 
 
 # Analogously, the estimators "WLSM" and "WLSMV" imply the "DWLS" estimator (not the "WLS" estimator) with robust standard errors and a mean or mean and variance adjusted test statistic. Estimators "ULSM" and "ULSMV" imply the "ULS" estimator with robust standard errors and a mean or mean and variance adjusted test statistic
-
 
 # Write a lavaan model:
 lavaan_cfa_model <- "
